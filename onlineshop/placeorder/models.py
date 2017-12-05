cb8aa343cf70375fe389130b82b5eb04a127a823
Esther <esther.lopezramos@estudiante.uam.es>
2017-12-05 19:07:43 +0100

    model Orderline

--- onlineshop/placeorder/models.py
+++ onlineshop/placeorder/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from shop.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    pass
+
+class Orderline(models.Model):
+    order=models.ForeignKey(Order, related_name='orderLines', null=False)
+    product=models.ForeignKey(Product, related_name='productLines', null=False)
+    unit=models.IntegerField(default=0, null=False)
+    pricePerUnit=models.DecimalField()
+    
+    def __str__(self):
+        return self.order + self.product
+    def __unicode__(self):
+        return self.order + self.product
+    
