4943b667af5c5b1889bee625bd4a64af6fce4269
Antonio Amor <antonio.amor.mourelle@gmail.com>
2017-12-05 19:06:22 +0100

    order model ADDED

--- onlineshop/placeorder/models.py
+++ onlineshop/placeorder/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Order(models.Model):
+    firstName=models.CharField( unique=False, null=False)
+    familyName=models.CharField( unique=False, null=False)
+    email=models.EmailField()
+    address=models.CharField(unique=False, null=False)
+    zip=models.CharField(unique=False, null=False)#postal code
+    city=models.CharField(unique=False, null=False)#postal code
+    created=models.DateTimeField(default=now)
+    updated=models.DateTimeField(default=now)
+    paid=models.BooleanField(default=False)
