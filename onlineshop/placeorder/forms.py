e893edb106b380a9243b9fbac3e1e6c7a4907676
Antonio Amor <antonio.amor.mourelle@gmail.com>
2017-12-05 19:27:32 +0100

    forms.py added

---
+++ onlineshop/placeorder/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class OrderCreateForm(forms.ModelForm):
+    choices = ((str(i), str(i)) for i in range(1,11))
+    units=forms.IntegerField(widget=forms.Select(choices=choices), min_value=1, max_value=10, initial=1)
+    update=forms.BooleanField(widget=forms.HiddenInput(), required=False)
